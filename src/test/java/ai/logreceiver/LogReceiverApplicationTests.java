e975f32c26ca608ba2b538a2c8ca2d0d38f79147
head1ton <head1ton@gmail.com>
2023-10-05 11:35:17 +0900

    Initial commit

---
+++ src/test/java/ai/logreceiver/LogReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.logreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogReceiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
