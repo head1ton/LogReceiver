6d2fa164a7b44936ea4ad19be4d0a7f13346672d
head1ton <head1ton@gmail.com>
2023-10-07 23:20:06 +0900

    ...

---
+++ consumers/src/main/java/ai/consumers/SimpleConsumer.java
@@ -0,0 +1,24 @@
+package ai.consumers;
+
+import java.util.List;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class SimpleConsumer {
+
+    public static void main(String[] args) {
+
+        String topicName = "simple-topic";
+
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "15.164.90.78:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
+        kafkaConsumer.subscribe(List.of(topicName));
+    }
+}
